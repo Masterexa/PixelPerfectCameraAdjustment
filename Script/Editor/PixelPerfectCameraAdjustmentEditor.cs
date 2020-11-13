ebe18bbaf1936d59acf7f7a2dcd82a7952625cbf
Masterexa <master.exa@gmail.com>
2020-11-13 12:53:23 +0900

    Add source codes

---
+++ Script/Editor/PixelPerfectCameraAdjustmentEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Kimiguna.Graphics2D;
+
+namespace KimigunaEditor.Graphics2D
+{
+    [CustomEditor(typeof(PixelPerfectCameraAdjustment))]
+    public class PixelPerfectCameraAdjustmentEditor : Editor
+    {
+        #region Fields
+            SerializedProperty m_matchMode;
+            SerializedProperty m_matchRatio;
+        #endregion
+
+        #region Event
+            private void OnEnable() {
+                m_matchMode = serializedObject.FindProperty(nameof(m_matchMode));
+                m_matchRatio = serializedObject.FindProperty(nameof(m_matchRatio));
+            }
+
+            public override void OnInspectorGUI()
+            {
+                DrawDefaultInspector();
+                if( m_matchMode.intValue==(int)PixelPerfectCameraAdjustment.MatchMode.WidthOrHeight )
+                using(var ind = new EditorGUI.IndentLevelScope())
+                {
+                    EditorGUILayout.PropertyField(m_matchRatio);
+                    
+                    var rc = EditorGUILayout.GetControlRect();
+
+                    rc.y -= EditorGUIUtility.singleLineHeight*0.5f;
+                    rc.xMin += EditorGUIUtility.labelWidth;
+                    GUI.Label(new Rect(rc.x,rc.y,50f,rc.height), "Width");
+                    rc.xMin = rc.xMax-100f;
+                    GUI.Label(new Rect(rc.x,rc.y,50f,rc.height), "Height");
+                }
+                serializedObject.ApplyModifiedProperties();
+
+                var cmp = (PixelPerfectCameraAdjustment)target;
+                if(Application.isPlaying)
+                {
+                    EditorGUILayout.Space();
+                    EditorGUILayout.HelpBox($"Computed Match Ratio : {cmp.computedMatchRatio}\nRaciprocal : {1f/Camera.main.aspect}", MessageType.Info);
+                }
+            }
+        #endregion
+    }
+}
---
+++ Script/Runtime/PixelPerfectCameraAdjustment.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace Kimiguna.Graphics2D{
+    [RequireComponent(typeof(UnityEngine.Experimental.Rendering.Universal.PixelPerfectCamera))]
+    public class PixelPerfectCameraAdjustment : MonoBehaviour
+    {
+        public enum MatchMode
+        {
+            WidthOrHeight,
+            Trim,
+            Extend,
+            Envelove,
+        }
+
+        #region Fields
+            [SerializeField]Vector2Int m_referenceResolution = new Vector2Int(400,300);
+            public Vector2Int referenceResolution
+            {
+                get => m_referenceResolution;
+                set => m_referenceResolution = value;
+            }
+
+            [Space]
+            [SerializeField]MatchMode m_matchMode = MatchMode.Extend;
+            public MatchMode matchMode
+            {
+                get => m_matchMode;
+                set => m_matchMode = value;
+            }
+
+            [HideInInspector,Range(0,1)]
+            [SerializeField]float m_matchRatio = 1f;
+            public float matchRatio
+            {
+                get => m_matchRatio;
+                set => m_matchRatio = Mathf.Clamp01(value);
+            }
+
+            UnityEngine.Experimental.Rendering.Universal.PixelPerfectCamera  m_pixelPerfectCamera;
+            Camera  m_camera;
+
+            public float computedMatchRatio{get; private set;}
+        #endregion
+
+        #region Events
+            void Awake()
+            {
+                m_pixelPerfectCamera = GetComponent<UnityEngine.Experimental.Rendering.Universal.PixelPerfectCamera>();
+                m_camera = GetComponent<Camera>();
+            }
+
+            void LateUpdate()
+            {
+                var res = m_referenceResolution;
+                var refAspect = (float)res.x/(float)res.y;
+                var camAspect = m_camera.aspect;
+                var reciprocalAspect = 1f/camAspect;
+
+                computedMatchRatio =
+                    (matchMode==MatchMode.Trim) ?  ( (refAspect<camAspect) ? 0f : 1f ):
+                    (matchMode==MatchMode.Extend) ?  ( (refAspect>camAspect) ? 0f : 1f ):
+                    (matchMode==MatchMode.Envelove) ?  Mathf.Clamp01( Mathf.Max(camAspect,reciprocalAspect) ):
+                    m_matchRatio
+                ;
+
+                m_pixelPerfectCamera.refResolutionX = (int)Mathf.Lerp(res.x, (float)res.y*camAspect, computedMatchRatio);
+                m_pixelPerfectCamera.refResolutionY = (int)Mathf.Lerp((float)res.x*reciprocalAspect, res.y, computedMatchRatio);
+
+                // Crop to less than half the resolution of the camera
+                m_pixelPerfectCamera.refResolutionX = Mathf.Min(m_pixelPerfectCamera.refResolutionX, m_camera.pixelWidth/2);
+                m_pixelPerfectCamera.refResolutionY = Mathf.Min(m_pixelPerfectCamera.refResolutionY, m_camera.pixelHeight/2);
+            }
+        #endregion
+    }
+}
